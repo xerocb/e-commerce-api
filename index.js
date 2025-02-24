75a8c34023950d36364ab47c55802fda38c7c541
xerocb <ciaransbrady@gmail.com>
2025-02-24 23:37:07 +0000

    Add user login

--- index.js
+++ index.js
@@ -4,10 +4,67 @@ dotenv.config();
 const express = require('express');
 const app = express();
 const port = process.env.PORT;
+const session = require('express-session');
 const bodyParser = require('body-parser');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 const registerRouter = require('./routes/register');
+const query = require('./db/index');
+
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
 
 app.use(bodyParser.json());
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    done(null, { id });
+});
+
+passport.use(new LocalStrategy(
+    async (username, password, done) => {
+        try {
+            // Search for user using email
+            const emailInUseQuery = await query(
+                `SELECT *
+                FROM users
+                WHERE email=$1`,
+                [username]
+            );
+
+            const emailInUse = emailInUseQuery.rows?.length > 0;
+
+            if (!emailInUse) {
+                return done(null, false);
+            } else {
+                const user = emailInUseQuery.rows[0];
+                if (user.password !== password) {
+                    return done(null, false);
+                } else {
+                    return done(null, user);
+                }
+            }
+        } catch (err) {
+            return done(err);
+        }
+    }
+));
+
+app.post('/login', 
+    passport.authenticate('local'),
+    (req, res) => {
+        res.status(200).send('Login successful');
+    }
+);
 
 app.use('/register', registerRouter);
 
